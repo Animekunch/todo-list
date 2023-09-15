03edaaa77fd2170b9e6200990401c49388934491
Animekunch <juscalvichkunch@gmail.com>
2023-09-15 03:00:25 +0100

    2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# todo-list
+# todo-list <br>
+<b>Issues</b> <br>
+Remove the extra line in css <br>
+
--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import addicon from "./add.svg"
+
+let nextId = 0;
 
 function App() {
+  const [item, setItem ] = useState("")
+  const [storms, setStorms] = useState([]);
+
+  function handleclick() {
+    setStorms([
+      ...storms,
+      { id: nextId++, item: item}
+    ])
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container">
+      <h1>Todo List</h1>
+        <div className="text-box">
+          <input
+            placeholder="Add to list of things to do"
+            value={item}
+            onChange={e => setItem(e.target.value)}
+           
+          /> <img
+          src={addicon}
+          alt="search"
+          onClick={handleclick}
+        />
+           
+        </div>
+        <div className="List">
+          <h2>List of Todos</h2>
+          <div className="todos">
+            <ol type="1">
+              {storms.map(storm => (
+                <li key={storm.id}>{storm.item}{' '}</li>
+              ))}
+            </ol>
+          </div>
+        </div>
+      </div>
+      </>
   );
 }
 
