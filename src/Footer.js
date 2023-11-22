02ede15a7974d5007e86228da2ff205af90d9c57
Animekunch <juscalvichkunch@gmail.com>
2023-11-22 21:35:20 +0100

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = ({length}) => {
   return (
     <div className='footer '>
-      <p>{length} List {length === 1 ? "item" : "items"}</p>
+      <p>{length} list {length === 1 ? "item" : "items"}</p>
     </div>
   )
 }
