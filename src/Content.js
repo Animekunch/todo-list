1d9ee91e223cd8920fb6807269d214452c794928
Animekunch <juscalvichkunch@gmail.com>
2023-11-22 21:33:11 +0100

    Content

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ import ItemList from "./ItemList";
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <div className="main">
-      {/*This code checks if the items array is not null before calling the length property. If the items array is null, it will return 0 and the length property will not be called.*/}
+      {/*This code checks if the item's array is not null before calling the length property. If the items array is null, it will return 0 and the length property will not be called.*/}
       {items && items.length ? (
         <ItemList
           items={items}
