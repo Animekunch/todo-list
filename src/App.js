bc3729b0d889e7fc1c2c9fb374c2c92ae4ba3667
Animekunch <juscalvichkunch@gmail.com>
2023-11-25 21:03:23 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import SearchItems from "./SearchItems";
 import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   // setitems array to save from local storage
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist")) || []
+    JSON.parse(localStorage.getItem("shoppinglist")),
+    []
   );
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
 
-  const setAndSaveItems = (newItems) => {
-    // then now set it to setItems
-    setItems(newItems);
-    //save to local storage so during refresh it will not go back to default
-    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem("shoppinglist", JSON.stringify(items));
+  }, [items]);
 
   const addItem = (item) => {
     //check the index of the last element which is -1
@@ -26,7 +24,7 @@ function App() {
     //new item
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
@@ -34,12 +32,12 @@ function App() {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
@@ -52,16 +50,14 @@ function App() {
 
   return (
     <div className="App">
-      <Header title="Todo List App" />
+      <Header title="groceries" />
 
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-
       <SearchItems search={search} setSearch={setSearch} />
-
       <Content
         items={
           items &&
